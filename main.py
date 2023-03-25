296c0683a9f6686fc03da99a82d870e35f9297f0
Alex <alex.grekulovic91@gmail.com>
2023-03-25 15:16:19 -0400

    redistribute percentages

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import math
 
 def get_weights(dataframe_size: int, partition_size: int) -> list:
-    '''Find the best weights to partition the dataframe based on the desired partition size. Weights are accurate to 2 decimal places.'''
+    '''Find the best weights to partition the dataframe based on the desired partition size. Weights are rounded to 2 decimal places.'''
     if partition_size >= dataframe_size:
         return [1]
 
@@ -9,12 +10,19 @@ def get_weights(dataframe_size: int, partition_size: int) -> list:
         # This happens when partition size requested is too small.
         # Setting weight to 1% for now
         weight = 0.01
-    num_partitions = int(1 / weight)
 
+    num_partitions = 1 / weight
+    if num_partitions.is_integer():
+        return [weight] * int(num_partitions)
+    
+    # Number of partitions has a decimal portion. Try to distribute weight evenly across partitions.
+    num_partitions = math.ceil(num_partitions)
+    weight = round(1 / num_partitions, 2)
     weights = [weight] * num_partitions
-    remainder = round(1 - sum(weights), 2)
-    if remainder > 0:
-        weights.append(remainder)
+
+    # If sum of weights is not equal to 1, adjust the last weight in weights with the difference.
+    diff = 1 - sum(weights)
+    weights.append(round(weights.pop() + diff, 2))
 
     return weights
 
